e18ca1d2600ff0073bc4d59fb094e3dd9fd046eb
ChristophHoltmann <christoph.holtmann@live.de>
2015-01-20 15:40:16 +0100

    rvwerv

---
+++ Cassida/Assets/Scripts/Helpful Scripts/Playerprefs.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public struct Layer
+{
+    public const string Default = "Default";
+    public const string TransparentFX = "TransparentFX";
+    public const string IgnoreRaycast = "Ignore Raycast";
+    public const string Water = "Water";
+    public const string UI = "UI";
+}
